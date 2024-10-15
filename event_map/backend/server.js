81aefbc0666e9bc8d4cf51d2bee9f12bc9d67c3a
stefanmallaun <stmallaun@tsn.at>
2024-10-15 21:34:53 +0200

    Pop-up field with data to db(event)

--- event_map/backend/server.js
+++ event_map/backend/server.js
@@ -30,7 +30,7 @@ app.post('/api/save-position', async (req, res) => {
 
     try {
         const [result] = await pool.query(
-            'INSERT INTO locations (address, latitude, longitude) VALUES (?,POINT(?, ?), POINT(?, ?))',
+            'INSERT INTO Location (address, latitude, longitude) VALUES (?,POINT(?, ?), POINT(?, ?))',
             [address, latitude, longitude, latitude, longitude]
         );
         res.status(200).json({ message: 'Position saved successfully', location_id: result.insertId });
@@ -39,6 +39,26 @@ app.post('/api/save-position', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+app.post('/api/save-eventdata', async (req, res) => {
+    console.log('Request body:', req.body); 
+    const {location_id, title, description, event_date, type_of_event} = req.body;
+
+    if (!location_id || !title || !description || !event_date || !type_of_event) {
+        return res.status(400).json({ message: 'location_id, title, description, event_date, and type_of_event are required' });
+    }
+
+    try {
+        const [result] = await pool.query(
+            'INSERT INTO Event (location_id, title, description, event_date, type_of_event) VALUES (?,?,?,?,?)',
+            [location_id, title, description, event_date, type_of_event]
+        );
+        res.status(200).json({ message: 'Event data saved successfully', event_id: result.insertId });
+    } catch (err) {
+        console.error('Error inserting event data:', err.message);
+        res.status(500).json({ error: err.message });
+    }
+});
+
 
 
 const PORT = process.env.PORT || 3000;
--- event_map/lib/main.dart
+++ event_map/lib/main.dart
@@ -27,15 +27,23 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
   final List<Marker> _markers = [];
   //WEB
-  //final String apiUrl = 'http://localhost:3000/api/save-position';
+  final String locationApiUrl = 'http://localhost:3000/api/save-position';
+  final String eventApiUrl = 'http://localhost:3000/api/save-eventdata';
   //ANDROID
-  final String apiUrl = 'http://10.0.2.2:3000/api/save-position';
+  //final String locationApiUrl = 'http://10.0.2.2:3000/api/save-position';
+  //final String eventApiUrl = 'http://10.0.2.2:3000/api/save-eventdata';
 
-  // Function to add marker
+  final _formKey = GlobalKey<FormState>();
+  String _title = '';
+  String _description = '';
+  DateTime? _eventDate;
+  int _eventType = 1; 
+
+  
   void _addMarker(LatLng position) {
     print('Marker added at: ${position.latitude}, ${position.longitude}');
-    _fetchAndSendData(position);
-    
+    _fetchAndSaveData(position);
+
     setState(() {
       _markers.add(
         Marker(
@@ -52,7 +60,7 @@ class MapSampleState extends State<MapSample> {
     });
   }
 
-  // Function to get address using Nominatim (OpenStreetMap)
+  // Get address from long and latitude
   Future<String> getNominatimAddress(double latitude, double longitude) async {
     final String url = 'https://nominatim.openstreetmap.org/reverse?format=json&lat=$latitude&lon=$longitude';
 
@@ -61,7 +69,7 @@ class MapSampleState extends State<MapSample> {
 
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-        return data['display_name'] ?? 'Unknown address';  // Return the address
+        return data['display_name'] ?? 'Unknown address'; 
       } else {
         print('Failed to retrieve address.');
         return 'Unknown address';
@@ -72,39 +80,192 @@ class MapSampleState extends State<MapSample> {
     }
   }
 
-  // Function to send position and address to the database
-  Future<void> _sendPositionToDatabase(LatLng position, String address) async {
+  // location-save & GET ID (for foreign key - event table)
+  Future<int?> _saveLocationToDatabase(LatLng position, String address) async {
     try {
       final response = await http.post(
-        Uri.parse(apiUrl),
+        Uri.parse(locationApiUrl),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
         body: jsonEncode(<String, dynamic>{
           'latitude': position.latitude,
           'longitude': position.longitude,
-          'address': address,  
+          'address': address,
         }),
       );
 
       if (response.statusCode == 200) {
-        print('Position and address saved successfully');
+        final data = json.decode(response.body);
+        return data['location_id']; 
       } else {
-        print('Failed to save position and address: ${response.body}');
+        print('Failed to save location: ${response.body}');
+        return null;
       }
     } catch (e) {
-      print('Error sending position and address to database: $e');
+      print('Error saving location: $e');
+      return null;
     }
   }
 
-  // Function to fetch the address and send both position and address to the database
-  Future<void> _fetchAndSendData(LatLng position) async {
-    // Get the address first
+  
+  Future<void> _saveEventToDatabase(int locationId) async {
+    try {
+      final response = await http.post(
+        Uri.parse(eventApiUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, dynamic>{
+          'location_id': locationId,
+          'title': _title,
+          'description': _description,
+          'event_date': _eventDate?.toIso8601String(),
+          'type_of_event': _eventType,
+          
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('Event data saved successfully');
+      } else {
+        print('Failed to save event data: ${response.body}');
+      }
+    } catch (e) {
+      print('Error saving event data: $e');
+    }
+  }
+
+  // GET-Address, Save location in DB, Save event in DB
+  Future<void> _fetchAndSaveData(LatLng position) async {
+    
     String address = await getNominatimAddress(position.latitude, position.longitude);
 
-    // Then send both the position and address to the database
-    await _sendPositionToDatabase(position, address);
+   
+    int? locationId = await _saveLocationToDatabase(position, address);
+
+    if (locationId != null) {
+      
+      await _saveEventToDatabase(locationId);
+    } else {
+      print('Failed to save location, event data not saved.');
+    }
   }
+
+  // Pop-up window to input Event-data
+  void _addEventData(LatLng position) {
+  DateTime? selectedDate = _eventDate; // Local variable for date within dialog
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setDialogState) { // StatefulBuilder for live updates in dialog
+          return AlertDialog(
+            title: const Text('Event Daten eingeben'),
+            content: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Titel'),
+                      onSaved: (value) {
+                        _title = value ?? '';
+                      },
+                      validator: (value) {
+                        return (value == null || value.isEmpty)
+                            ? 'Bitte Titel eingeben'
+                            : null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Beschreibung'),
+                      onSaved: (value) {
+                        _description = value ?? '';
+                      },
+                      validator: (value) {
+                        return (value == null || value.isEmpty)
+                            ? 'Bitte Beschreibung eingeben'
+                            : null;
+                      },
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          selectedDate == null
+                              ? 'Kein Datum ausgewählt'
+                              : 'Datum: ${selectedDate.toString().split(' ')[0]}',
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.calendar_today),
+                          onPressed: () async {
+                            DateTime? date = await showDatePicker(
+                              context: context,
+                              initialDate: selectedDate ?? DateTime.now(),
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(2101),
+                            );
+                            if (date != null) {
+                              // Update dialog UI using setDialogState
+                              setDialogState(() {
+                                selectedDate = date;
+                              });
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                    DropdownButtonFormField<int>(
+                        decoration: const InputDecoration(labelText: 'Event-Typ'),
+                        value: _eventType,
+                        items: const [
+                          DropdownMenuItem(child: Text('Keine Angabe'), value: 1),
+                          DropdownMenuItem(child: Text('Sport'), value: 2),
+                          DropdownMenuItem(child: Text('Party'), value: 3),
+                          DropdownMenuItem(child: Text('Concert'), value: 4),
+                        ],
+                        onChanged: (value) {
+                          setDialogState(() {
+                            if (value != null) {
+                              _eventType = value; 
+                            }
+                          });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Abbrechen'),
+                onPressed: () {
+                  Navigator.of(context).pop(); // Close dialog
+                },
+              ),
+              TextButton(
+                child: const Text('Bestätigen'),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    setState(() {
+                      _eventDate = selectedDate; // Save the selected date to state
+                    });
+                    Navigator.of(context).pop(); // Close the dialog
+                    _addMarker(position); // Add the marker and save the data
+                  }
+                },
+              ),
+            ],
+          );
+        },
+      );
+    },
+  );
+}
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -114,10 +275,10 @@ class MapSampleState extends State<MapSample> {
       ),
       body: FlutterMap(
         options: MapOptions(
-          center: LatLng(51.509364, -0.128928), 
+          center: LatLng(51.509364, -0.128928),
           zoom: 13.0,
           onTap: (tapPosition, latlng) {
-            _addMarker(latlng); 
+            _addEventData(latlng); 
           },
         ),
         children: [
@@ -126,7 +287,7 @@ class MapSampleState extends State<MapSample> {
             subdomains: const ['a', 'b', 'c'],
           ),
           MarkerLayer(
-            markers: _markers, 
+            markers: _markers,
           ),
         ],
       ),
